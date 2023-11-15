d6b63a18f2c5407498f197bdb7ae4bbf85252aca
charescape <53265646+charescape@users.noreply.github.com>
2023-11-15 17:52:16 +0800

    first commit

---
+++ ecs.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use PhpCsFixer\Fixer\Import\NoUnusedImportsFixer;
+use PhpCsFixer\Fixer\Strict\DeclareStrictTypesFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->paths([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ]);
+
+    // run and fix, one by one
+    $ecsConfig->import(SetList::ARRAY);
+    $ecsConfig->import(SetList::DOCBLOCK);
+    $ecsConfig->import(SetList::PSR_12);
+
+    $ecsConfig->ruleWithConfiguration(ArraySyntaxFixer::class, [
+        'syntax' => 'short'
+    ]);
+    $ecsConfig->rule(NoUnusedImportsFixer::class);
+    $ecsConfig->rule(DeclareStrictTypesFixer::class);
+
+
+
+};
